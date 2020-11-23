e82c28b422abc4e886dcbad2fd120b2c2fad7a8c
tenntenn <uedatakuya@gmail.com>
2020-11-23 16:12:03 +0900

    Add IsGeneratedFile

--- file.go
+++ file.go
@@ -3,6 +3,7 @@ package analysisutil
 import (
 	"go/ast"
 	"go/token"
+	"regexp"
 
 	"golang.org/x/tools/go/analysis"
 )
@@ -15,4 +16,20 @@ func File(pass *analysis.Pass, pos token.Pos) *ast.File {
 		}
 	}
 	return nil
+}
+
+var genCommentRegexp = regexp.MustCompile(`^// Code generated .* DO NOT EDIT\.$`)
+
+// IsGeneratedFile reports whether the file has been generated automatically.
+// If file is nil, IsGeneratedFile will return false.
+func IsGeneratedFile(file *ast.File) bool {
+	if file == nil || file.Doc == nil {
+		return false
+	}
+	return genCommentRegexp.MatchString(file.Doc.List[0].Text)
+}
+
+// IsGenerated reports whether a file which include pos has been generated automatically.
+func IsGenerated(pass *analysis.Pass, pos token.Pos) bool {
+	return IsGeneratedFile(File(pass, pos))
 }
